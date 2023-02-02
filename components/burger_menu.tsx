6729d45aa4a070efec6ab61b75a290607eda57af
Jeff <jeff.kinossian@gmail.com>
2023-02-02 20:08:18 +0100

    update2 navbar

--- components/burger_menu.tsx
+++ components/burger_menu.tsx
@@ -1,27 +1,24 @@
 const BurgerMenu = ({isOpen}: any) => {
-    const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-indigo-900 transition ease transform duration-300`;
+    const colorDark = "indigo-900";
+    const colorClair = "indigo-50";
+
+    const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-${colorDark} transition ease transform duration-300`;
 
     return (
-        <button className="flex flex-col h-12 w-12 border-none border-indigo-900 rounded justify-center items-center group">
+        <button
+            className={`flex flex-col h-12 w-12 border-none border-${colorDark} rounded justify-center items-center group`}
+        >
             <div
                 className={`${genericHamburgerLine} ${
-                    isOpen
-                        ? "rotate-45 translate-y-3 opacity-100 group-hover:opacity-100 bg-indigo-50"
-                        : "opacity-100 group-hover:opacity-100"
+                    isOpen && `rotate-45 translate-y-3 bg-${colorClair}`
                 }`}
             />
             <div
-                className={`${genericHamburgerLine} ${
-                    isOpen
-                        ? "opacity-0 bg-indigo-opacity-100"
-                        : "opacity-100 group-hover:opacity-100"
-                }`}
+                className={`${genericHamburgerLine} ${isOpen && "opacity-0 "}`}
             />
             <div
                 className={`${genericHamburgerLine} ${
-                    isOpen
-                        ? "-rotate-45 -translate-y-3 opacity-100 group-hover:opacity-100 bg-indigo-50"
-                        : "opacity-100 group-hover:opacity-100"
+                    isOpen && `-rotate-45 -translate-y-3 bg-${colorClair}`
                 }`}
             />
         </button>
