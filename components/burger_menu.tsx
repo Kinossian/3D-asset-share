4de282d48168739ee3a2038a7c72f829c3cd14b2
Jeff <jeff.kinossian@gmail.com>
2023-02-03 12:00:06 +0100

    update

--- components/burger_menu.tsx
+++ components/burger_menu.tsx
@@ -1,24 +1,23 @@
 const BurgerMenu = ({isOpen}: any) => {
-    const colorDark = "indigo-900";
-    const colorClair = "indigo-50";
-
-    const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-${colorDark} transition ease transform duration-300`;
+    const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-indigo-${
+        isOpen ? "300" : "900"
+    } transition ease transform duration-300`;
 
     return (
         <button
-            className={`flex flex-col h-12 w-12 border-none border-${colorDark} rounded justify-center items-center group`}
+            className={`flex flex-col h-12 w-12 rounded justify-center items-center group`}
         >
             <div
                 className={`${genericHamburgerLine} ${
-                    isOpen && `rotate-45 translate-y-3 bg-${colorClair}`
+                    isOpen && `rotate-45 translate-y-3 `
                 }`}
             />
             <div
-                className={`${genericHamburgerLine} ${isOpen && "opacity-0 "}`}
+                className={`${genericHamburgerLine} ${isOpen && "opacity-0"}`}
             />
             <div
                 className={`${genericHamburgerLine} ${
-                    isOpen && `-rotate-45 -translate-y-3 bg-${colorClair}`
+                    isOpen && `-rotate-45 -translate-y-3 `
                 }`}
             />
         </button>
